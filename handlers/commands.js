438ee95268ae947d60cb9b4d237c5e028f219a46
4 <104457277+4alaska@users.noreply.github.com>
2024-11-13 14:16:27 +0100

    Update commands.js

--- handlers/commands.js
+++ handlers/commands.js
@@ -1,1 +1,27 @@
+const { readdirSync } = require('fs');
+const {greenBright, redBright, yellow} = require("cli-color");
 
+module.exports = client => {
+
+    console.log(yellow.underline("Chargement des commandes..."))
+
+    const dirscmd = readdirSync('./commands/')
+
+    for (const dirsdoss of dirscmd) {
+        console.log(yellow.bold(`> ${dirsdoss}`))
+
+        const filesdirs = readdirSync(`./commands/${dirsdoss}/`).filter(file => file.endsWith('.js'))
+
+        for(const files of filesdirs) {
+            const cmd = require(`../commands/${dirsdoss}/${files}`)
+            
+            if(!cmd.data) return console.log(redBright(`  > La commande ${files} n'a pas de nom !`))
+            cmd.category = dirsdoss.toLowerCase();
+        
+            client.commands.set(cmd.data.name, cmd)
+
+            console.log(yellow(`  > ${files}`))
+
+        }
+    }
+}
